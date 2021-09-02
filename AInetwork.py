411e5622d1f058cce694bacddafbd9b16eb3b338
Hananel Hadad <chananel94@gmail.com>
2021-09-02 14:40:01 +0300

    Update AInetwork.py
    c

--- AInetwork.py
+++ AInetwork.py
@@ -9,6 +9,26 @@ import matplotlib.pyplot as plt
 
 embedding_dim = 50
 
+
+class MyBilstm(nn.Module):
+    def __init__(self):
+        super(MyBilstm, self).__init__()
+        self.embedding = nn.Embedding.from_pretrained(self.createEmb(), norm_type=2, max_norm=2).requires_grad_(True)
+        self.lstm = nn.LSTM(input_size=emb_dim, hidden_size=hid_dim, num_layers=2, bidirectional=True)
+        self.dropout = nn.Dropout()
+        self.fc = nn.Linear(hid_dim * 2, len(vocabL.values()))
+
+    def forward(self, x):  # (sampleLen,)
+        emb = self.embedding(x)  # (sampleLen, embDim)
+        hn, _ = self.lstm(emb.view(len(x), 1, -1))  # (sampleLen, batchSize, hidDim*2)
+        hn = self.dropout(hn)
+        hn = torch.squeeze(hn)  # (sampleLen, hidDim*2)
+        out = self.fc(hn)  # (len(vocabL.values()), )
+        if len(x) == 1:  # the output must has 2 dimensions for loss calculation
+            out = out.reshape(1, -1)
+        return out
+    
+    
 # The neural network
 class MyModel(nn.Module):
     def __init__(self):
