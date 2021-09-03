952ecb23f5d68f7ea8d28c6ef1b31127a8721948
Hananel <chananel94@gmail.com>
2021-09-03 12:53:20 +0300

    network running

--- AInetwork.py
+++ AInetwork.py
@@ -1,18 +1,18 @@
-import numpy as np
-import sys
 import torch
 import torch.nn as nn
 from torch import optim
 from torch.utils.data import DataLoader
-from torch.nn import CrossEntropyLoss
-import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from AI_utils import *
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 emb_dim = 50
 hid_dim = 100
 batchSize = 1
 lr = 0.01
-epochs = 10
+epochs = 5
 
 
 # The amazing neural network
@@ -20,13 +20,14 @@ class MyModel(nn.Module):
     def __init__(self):
         super(MyModel, self).__init__()
         # maps each label to an embedding_dim vector
-        self.embeddings = nn.Embedding(len(vocabW), emb_dim).requires_grad_(True)
+        self.embeddings = nn.Embedding(len(vocab), emb_dim).requires_grad_(True)
         self.lstm = nn.LSTM(input_size=emb_dim, hidden_size=hid_dim, num_layers=3)
         self.fc = nn.Linear(hid_dim, 1)
 
     def forward(self, x):
-        emb = self.embedding(x)  # (sampleLen, embDim)
-        hn, _ = self.lstm(emb.view(len(x), 1, hid_dim))  # (sampleLen, batchSize=1, hidDim)
+        x = torch.squeeze(x)
+        emb = self.embeddings(x)  # (sampleLen, embDim)
+        hn, _ = self.lstm(emb.view(len(emb), 1, -1))  # (sampleLen, batchSize=1, hidDim)
         out = self.fc(hn[-1][-1])  # (1,)
         return torch.sigmoid(out)
 
@@ -66,57 +67,27 @@ def dev(list_of_tuples):
     return accuracy, lossTotal
 
 
-# plot 2 given lists of values
-def plotMeasurement(measurement, trainMeasure, devMeasure):
-    epochsList = [i for i in range(epochs)]
-    plt.figure()
-    plt.title(measurement)
-    plt.plot(epochsList, trainMeasure, label="Train")
-    plt.plot(epochsList, devMeasure, label="Dev")
-    plt.xlabel("Epochs")
-    plt.ylabel(measurement)
-    plt.locator_params(axis="x", integer=True, tight=True)  # make x axis to display only whole number (iterations)
-    plt.legend()
-    plt.savefig(measurement)
-
-
-# Create vocabulary dict of words or labels
-def createVocab():
-    dict = {}
-    i = 0
-
-    # add words from file
-    with open("vocab.txt", "r") as f:
-        for word in f:
-            word = word.strip()
-            dict[word] = i
-            i += 1
-
-    # add special tokens
-    dict['PAD_BEGIN'] = len(dict)
-    dict['PAD_END'] = len(dict)
-
-    return dict
-
 
 if __name__ == '__main__':
     # read data and vocab from files
-    data = pd.read_excel("data.xlsx")
+    data = pd.read_excel("data.xlsx")       # change to csv later
     sentences, labels = data.pop("sentences"), data.pop("labels")
     vocab = createVocab()
-    # convert to numbers
-    labels = [0 if label == "Real" else 1 for label in labels]
-    sentences = [sentence.split(' ') for sentence in sentences]
-    sentences = [[word.lower() for word in sentence] for sentence in sentences]   # delete when receive clean data
-    sentences = [[vocab[word] for word in sentence] for sentence in sentences]
 
+    # clean sentences, add word to vocab
+    sentences = [stripToTextVector(sentence) for sentence in sentences]
+    for sentence in sentences:
+        for word in sentence:
+            if word not in vocab:
+                vocab[word] = len(vocab)
 
-    trainData = 1  # [([list of words], label)]
-    devData = 1
-    vocabW = 1  # {word : serial number}
+    # encode, split to train and dev
+    sentences, labels = myEncode(sentences, labels, vocab)
+    allData = [(sentence, label) for sentence, label in zip(sentences, labels)]  # [([list of words], label)]
+    trainData, devData = train_test_split(allData)
 
     # model
-    model = MyModel()
+    model = MyModel().to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     loss_f = nn.BCELoss()
 
@@ -132,5 +103,7 @@ if __name__ == '__main__':
         accuracies_dev.append(accuracy_dev)
 
     # plot results
-    plotMeasurement("Loss", losses_train, losses_dev)
-    plotMeasurement("Accuracy", accuracies_train, accuracies_dev)
+    plotMeasurement("Loss", losses_train, losses_dev, epochs)
+    plotMeasurement("Accuracy", accuracies_train, accuracies_dev, epochs)
+
+    torch.save(model, "modelFile")
